4962cc298020985bbcd1755fa0079cd1f4662a05
lordhasyim <lordhasyim@gmail.com>
2018-07-04 14:19:43 +0700

    add generated docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","createuriFromServer()"],["f","createUriFromString()"],["c","One\\Collection"],["c","One\\DummyLogger"],["c","One\\JsonInterface"],["c","One\\Model\\Article"],["c","One\\Model\\Gallery"],["c","One\\Model\\Model"],["c","One\\Model\\Page"],["c","One\\Model\\Photo"],["c","One\\Model\\Video"],["c","One\\Publisher"],["c","One\\ToArrayInterface"],["c","One\\Uri"]];
