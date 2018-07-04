4e10e6d290da32befbd03c8b7b4c71989b8b93e1
Charis <me@chars.me>
2018-07-04 16:38:59 +0700

    Move env load for test, clean composer.json script and autoloader

--- test/Unit/PublisherTest.php
+++ test/Unit/PublisherTest.php
@@ -16,41 +16,22 @@ class PublisherTest extends \PHPUnit\Framework\TestCase
 
     public function setUp()
     {
-        $envPath = realpath(__DIR__ . '/../.env');
-        if (!file_exists($envPath)) {
+        $env = \loadTestEnv();
+        if (empty($env)) {
             $this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on $envPath to run test");
         }
 
-        $env = array_reduce(
-            array_filter(
-                explode(
-                    "\n",
-                    file_get_contents($envPath)
-                )
-            ),
-            function ($carry, $item) {
-                list($key, $value) = explode('=', $item, 2);
-                $carry[$key] = $value;
-                return $carry;
-            },
-            array()
-        );
-
         $this->publisher = new Publisher(
             $env['CLIENT_ID'],
-            $env['CLIENT_SECRET'],
-            !empty($env['ACCESS_TOKEN']) ?
-            array(
-                'access_token' => $env['ACCESS_TOKEN'],
-            ) : array()
+            $env['CLIENT_SECRET']
         );
     }
 
     public function testAuthentication()
     {
-        $json_response = $this->publisher->listArticle();
+        $jsonResponse = $this->publisher->listArticle();
 
-        $data = json_decode($json_response, true);
+        $data = json_decode($jsonResponse, true);
 
         $this->assertArrayNotHasKey('message', $data);
 
@@ -61,36 +42,21 @@ class PublisherTest extends \PHPUnit\Framework\TestCase
 
     public function testRecycleToken()
     {
-        $envPath = realpath(__DIR__ . '/../.env');
+        $env = \loadTestEnv();
 
-        if (!file_exists($envPath)) {
-            $this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on $envPath to run test");
+        if (empty($env['ACCESS_TOKEN'])) {
+            $this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on /test/.env to run test");
         }
 
-        $token_producer = function () use ($envPath) {
-            $env = array_reduce(
-                array_filter(
-                    explode(
-                        "\n",
-                        file_get_contents($envPath)
-                    )
-                ),
-                function ($carry, $item) {
-                    list($key, $value) = explode('=', $item, 2);
-                    $carry[$key] = $value;
-                    return $carry;
-                },
-                array()
-            );
-
+        $tokenProducer = function () use ($env) {
             return $env['ACCESS_TOKEN'];
         };
 
-        $this->publisher->recycleToken($token_producer);
+        $this->publisher->recycleToken($tokenProducer);
 
-        $json_response = $this->publisher->listArticle();
+        $jsonResponse = $this->publisher->listArticle();
 
-        $data = json_decode($json_response, true);
+        $data = json_decode($jsonResponse, true);
 
         $this->assertArrayNotHasKey('message', $data);
 
