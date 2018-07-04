80b55482293e953bba25334427311eaf1e17784b
kenny <kennykarnama@gmail.com>
2018-07-04 16:52:46 +0700

    scrutinizer issue

--- src/FormatMapping.php
+++ src/FormatMapping.php
@@ -4,91 +4,97 @@ namespace one;
 
 use One\Model\Article;
 
-class FormatMapping {
+class FormatMapping
+{
 
-	/**
-	 * map a single article to main attributes in Article Class
-	 * @param  string $singleJsonArticle JSON response
-	 * @return Article\Exception
-	 */
-	public function article($singleJsonArticle) {
-		if ($this->jsonToArray($singleJsonArticle)) {
-			$dataArticle = $this->jsonToArray($singleJsonArticle)['data'];
+    /**
+     * map a single article to main attributes in Article Class
+     * @param  string $singleJsonArticle JSON response
+     * @return Article\Exception
+     */
+    public function article($singleJsonArticle)
+    {
+        if ($this->jsonToArray($singleJsonArticle)) {
+            $dataArticle = $this->jsonToArray($singleJsonArticle)['data'];
 
-			$article = new Article(
-				$title = $this->filterString($this->getValue('title', $dataArticle)),
+            $article = new Article(
+                $title = $this->filterString($this->getValue('title', $dataArticle)),
 
-				$body = $this->filterString($this->getValue('body', $dataArticle)),
+                $body = $this->filterString($this->getValue('body', $dataArticle)),
 
-				$source = $this->filterString($this->getValue('source', $dataArticle)),
+                $source = $this->filterString($this->getValue('source', $dataArticle)),
 
-				$uniqueId = $this->getValue('unique_id', $dataArticle),
+                $uniqueId = $this->getValue('unique_id', $dataArticle),
 
-				$typeId = $this->filterInteger($this->getValue('type_id', $dataArticle['type'])),
+                $typeId = $this->filterInteger($this->getValue('type_id', $dataArticle['type'])),
 
-				$categoryId = $this->filterInteger($this->getValue('category_id', $dataArticle['category'])),
+                $categoryId = $this->filterInteger($this->getValue('category_id', $dataArticle['category'])),
 
-				$reporter = $this->getValue('reporter', $dataArticle),
+                $reporter = $this->getValue('reporter', $dataArticle),
 
-				$lead = $this->filterString($this->getValue('lead', $dataArticle)),
+                $lead = $this->filterString($this->getValue('lead', $dataArticle)),
 
-				$tags = $this->getValue('tag_name', $dataArticle['tags']),
+                $tags = $this->getValue('tag_name', $dataArticle['tags']),
 
-				$publishedAt = $this->filterString($this->getValue('published_at', $dataArticle)),
+                $publishedAt = $this->filterString($this->getValue('published_at', $dataArticle)),
 
-				$identifier = $this->filterInteger($this->getValue('id', $dataArticle))
-			);
+                $identifier = $this->filterInteger($this->getValue('id', $dataArticle))
+            );
 
-			return $article;
-		}
+            return $article;
+        }
 
-		throw new \Exception("Empty or invalid JSON Response", 1);
-	}
+        throw new \Exception("Empty or invalid JSON Response", 1);
+    }
 
-	/**
-	 * Make sure value is integer
-	 * @param  int $int
-	 * @return boolean
-	 */
-	private function filterInteger($int) {
-		if (is_int((int) $int)) {
-			return $int;
-		}
-		throw new \Exception("Invalid Integer", 1);
-	}
+    /**
+     * Make sure value is integer
+     * @param  int $int
+     * @return boolean
+     */
+    private function filterInteger($int)
+    {
+        if (is_int((int) $int)) {
+            return $int;
+        }
+        throw new \Exception("Invalid Integer", 1);
+    }
 
-	/**
-	 * Make sure string is not null or empty
-	 * @param   mixed $str
-	 * @return string if it is valid or exception
-	 */
-	private function filterString($str) {
-		if (is_string($str) && strlen($str) > 0 && !is_null($str)) {
-			return $str;
-		}
-		throw new \Exception("String required", 1);
-	}
+    /**
+     * Make sure string is not null or empty
+     * @param   mixed $str
+     * @return string if it is valid or exception
+     */
+    private function filterString($str)
+    {
+        if (is_string($str) && strlen($str) > 0 && !is_null($str)) {
+            return $str;
+        }
+        throw new \Exception("String required", 1);
+    }
 
-	/**
-	 * Get value of array based on attributes(keys)
-	 * @param  supported php variables $attribute
-	 * @param  array $data
-	 * @return supported php variables
-	 */
-	private function getValue($attribute, $data) {
-		return isset($data[$attribute]) ? $data[$attribute] : null;
-	}
+    /**
+     * Get value of array based on attributes(keys)
+     * @param  supported php variables $attribute
+     * @param  array $data
+     * @return supported php variables
+     */
+    private function getValue($attribute, $data)
+    {
+        return isset($data[$attribute]) ? $data[$attribute] : null;
+    }
 
-	/**
-	 * Convert JSON string to associative array
-	 * @param  string $jsonResponse
-	 * @return array if it is valid json, null otherwise
-	 */
-	public function jsonToArray($jsonResponse) {
-		try {
-			return json_decode($jsonResponse, true);
-		} catch (\Exception $e) {
-			return null;
-		}
-	}
+    /**
+     * Convert JSON string to associative array
+     * @param  string $jsonResponse
+     * @return array if it is valid json, null otherwise
+     */
+    public function jsonToArray($jsonResponse)
+    {
+        try {
+            return json_decode($jsonResponse, true);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
 }
--- test/Unit/FormatMappingTest.php
+++ test/Unit/FormatMappingTest.php
@@ -5,75 +5,79 @@ namespace One\Test\Unit;
 use One\FormatMapping;
 use One\Publisher;
 
-class FormatMappingTest extends \PHPUnit\Framework\TestCase {
-	protected $formatMapping;
+class FormatMappingTest extends \PHPUnit\Framework\TestCase
+{
+    protected $formatMapping;
 
-	private $publisher;
+    private $publisher;
 
-	public function setUp() {
-		$envPath = realpath(__DIR__ . '/../.env');
-		if (!file_exists($envPath)) {
-			$this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on $envPath to run test");
-		}
+    public function setUp()
+    {
+        $envPath = realpath(__DIR__ . '/../.env');
+        if (!file_exists($envPath)) {
+            $this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on $envPath to run test");
+        }
 
-		$env = array_reduce(
-			array_filter(
-				explode(
-					"\n",
-					file_get_contents($envPath)
-				)
-			),
-			function ($carry, $item) {
-				list($key, $value) = explode('=', $item, 2);
-				$carry[$key] = $value;
-				return $carry;
-			},
-			array()
-		);
+        $env = array_reduce(
+            array_filter(
+                explode(
+                    "\n",
+                    file_get_contents($envPath)
+                )
+            ),
+            function ($carry, $item) {
+                list($key, $value) = explode('=', $item, 2);
+                $carry[$key] = $value;
+                return $carry;
+            },
+            array()
+        );
 
-		$this->publisher = new Publisher(
-			$env['CLIENT_ID'],
-			$env['CLIENT_SECRET'],
-			!empty($env['ACCESS_TOKEN']) ?
-			array(
-				'access_token' => $env['ACCESS_TOKEN'],
-			) : array()
-		);
+        $this->publisher = new Publisher(
+            $env['CLIENT_ID'],
+            $env['CLIENT_SECRET'],
+            !empty($env['ACCESS_TOKEN']) ?
+            array(
+                'access_token' => $env['ACCESS_TOKEN'],
+            ) : array()
+        );
 
-		$this->formatMapping = new FormatMapping();
-	}
+        $this->formatMapping = new FormatMapping();
+    }
 
-	public function testMapMainArticle() {
-		$idArticle = 2859;
+    public function testMapMainArticle()
+    {
+        $idArticle = 2859;
 
-		$jsonArticle = $this->publisher->getArticle($idArticle);
+        $jsonArticle = $this->publisher->getArticle($idArticle);
 
-		$this->assertArrayHasKey('data', json_decode($jsonArticle, true));
+        $this->assertArrayHasKey('data', json_decode($jsonArticle, true));
 
-		$article = $this->formatMapping->article($jsonArticle);
+        $article = $this->formatMapping->article($jsonArticle);
 
-		$this->assertNotNull($article);
+        $this->assertNotNull($article);
 
-		$this->assertEquals($idArticle, $article->getId());
-	}
+        $this->assertEquals($idArticle, $article->getId());
+    }
 
-	public function testJsonToArray() {
-		$json = '
+    public function testJsonToArray()
+    {
+        $json = '
 				{
   				"nama":"kenny karnama"
   				}
   				';
 
-		$isValid = $this->formatMapping->jsonToArray($json);
+        $isValid = $this->formatMapping->jsonToArray($json);
 
-		$this->assertNotNull($isValid);
+        $this->assertNotNull($isValid);
 
-		$json = array(
-			'nama' => 'kenny karnama',
-		);
+        $json = array(
+            'nama' => 'kenny karnama',
+        );
 
-		$isValid = $this->formatMapping->jsonToArray($json);
+        $isValid = $this->formatMapping->jsonToArray($json);
 
-		$this->assertNull($isValid);
-	}
+        $this->assertNull($isValid);
+    }
 }
