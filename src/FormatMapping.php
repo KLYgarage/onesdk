f9fafe9d9fc75ba12d9049ad081135e331218227
kenny <kennykarnama@gmail.com>
2018-07-04 14:09:08 +0700

    Add mapping function that maps json response of single article without attachment to be an article entity

---
+++ src/FormatMapping.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace one;
+
+use One\Model\Article;
+
+class FormatMapping {
+
+	/**
+	 * map a single article to main attributes in Article Class
+	 * @param  string $singleJsonArticle JSON response
+	 * @return \One\Model\Article                   An Article Object
+	 */
+	public function mapMainArticle($singleJsonArticle) {
+
+		$decoded_article = $singleJsonArticle;
+
+		if (!is_null($this->jsonToArray($singleJsonArticle))) {
+			$decoded_article = $this->jsonToArray($singleJsonArticle);
+		}
+
+		$data_article = $decoded_article['data'];
+
+		$title = function () use ($data_article) {
+			if (isset($data_article['title'])) {
+				return $data_article['title'];
+			}
+			return null;
+		};
+
+		$body = function () use ($data_article) {
+			if (isset($data_article['body'])) {
+				return $data_article['body'];
+			}
+			return null;
+		};
+
+		$source = function () use ($data_article) {
+			if (isset($data_article['source'])) {
+				return $data_article['source'];
+			}
+			return null;
+		};
+
+		$uniqueId = function () use ($data_article) {
+			if (isset($data_article['uniqueId'])) {
+				return $data_article['uniqueId'];
+			}
+			return null;
+		};
+
+		$typeId = function () use ($data_article) {
+			if (isset($data_article['type']['type_id'])) {
+				return $data_article['type']['type_id'];
+			}
+			return null;
+		};
+
+		$categoryId = function () use ($data_article) {
+			if (isset($data_article['category']['category_id'])) {
+				return $data_article['category']['category_id'];
+			}
+			return null;
+		};
+
+		$reporter = function () use ($data_article) {
+			if (isset($data_article['reporter'])) {
+				return $data_article['reporter'];
+			}
+			return null;
+		};
+
+		$lead = function () use ($data_article) {
+			if (isset($data_article['lead'])) {
+				return $data_article['lead'];
+			}
+			return null;
+		};
+
+		$tags = function () use ($data_article) {
+			if (isset($data_article['tags']['tag_name'])) {
+				return $data_article['tags']['tag_name'];
+			}
+			return null;
+		};
+
+		$publishedAt = function () use ($data_article) {
+			if (isset($data_article['published_at'])) {
+				return $data_article['published_at'];
+			}
+			return null;
+		};
+
+		$identifier = function () use ($data_article) {
+			if (isset($data_article['id'])) {
+				return $data_article['id'];
+			}
+			return null;
+		};
+
+		$article = array();
+
+		$article = new Article(
+
+			$title(),
+
+			$body(),
+
+			$source(),
+
+			$uniqueId(),
+
+			$typeId(),
+
+			$categoryId(),
+
+			$reporter(),
+
+			$lead(),
+
+			$tags(),
+
+			$publishedAt(),
+
+			$identifier()
+
+		);
+
+		return $article;
+
+	}
+
+	/**
+	 * Convert JSON string to associative array
+	 * @param  string $jsonResponse
+	 * @return array if it is valid json, null otherwise
+	 */
+	public function jsonToArray($jsonResponse) {
+
+		try {
+			return json_decode($jsonResponse, TRUE);
+
+		} catch (\Exception $e) {
+			return null;
+		}
+
+	}
+
+}
---
+++ test/Unit/FormatMappingTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace One\Test\Unit;
+
+use One\FormatMapping;
+use One\Publisher;
+
+class FormatMappingTest extends \PHPUnit\Framework\TestCase {
+
+	protected $formatMapping;
+
+	private $publisher;
+
+	public function setUp() {
+
+		$envPath = realpath(__DIR__ . '/../.env');
+		if (!file_exists($envPath)) {
+			$this->markTestSkipped("no .env defined. Need client ID and secret to continue this test, modify .env.example to .env on $envPath to run test");
+		}
+
+		$env = array_reduce(
+			array_filter(
+				explode(
+					"\n",
+					file_get_contents($envPath)
+				)
+			),
+			function ($carry, $item) {
+				list($key, $value) = explode('=', $item, 2);
+				$carry[$key] = $value;
+				return $carry;
+			},
+			array()
+		);
+
+		$this->publisher = new Publisher(
+			$env['CLIENT_ID'],
+			$env['CLIENT_SECRET'],
+			!empty($env['ACCESS_TOKEN']) ?
+			array(
+				'access_token' => $env['ACCESS_TOKEN'],
+			) : array()
+		);
+
+		$this->formatMapping = new FormatMapping();
+	}
+
+	public function testMapMainArticle() {
+
+		$id_article = 2859;
+
+		$json_article = $this->publisher->getArticle($id_article);
+
+		$this->assertArrayHasKey('data', json_decode($json_article, TRUE));
+
+		$article = $this->formatMapping->mapMainArticle($json_article);
+
+		$this->assertNotNull($article);
+
+		$this->assertEquals($id_article, $article->getId());
+
+	}
+
+	public function testJsonToArray() {
+		# code...
+		$json = '
+				{
+  				"nama":"kenny karnama"
+  				}
+  				';
+
+		$isValid = $this->formatMapping->jsonToArray($json);
+
+		$this->assertNotNull($isValid);
+
+		$json = array(
+			'nama' => 'kenny karnama',
+		);
+
+		$isValid = $this->formatMapping->jsonToArray($json);
+
+		$this->assertNull($isValid);
+	}
+}
