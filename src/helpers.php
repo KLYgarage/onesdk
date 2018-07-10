df586923c2e32a56157831ce5e13109a3006bdb8
kenny <kennykarnama@gmail.com>
2018-07-10 16:38:03 +0700

    fix scrutinizer issue

--- src/helpers.php
+++ src/helpers.php
@@ -96,18 +96,16 @@ function stream_for($resource = '', array $options = [])
         case 'object':
             if ($resource instanceof StreamInterface) {
                 return $resource;
-            } elseif ($resource instanceof \Iterator) {
-                return new PumpStream(function () use ($resource) {
-                    if (!$resource->valid()) {
-                        return false;
-                    }
-                    $result = $resource->current();
-                    $resource->next();
-                    return $result;
-                }, $options);
             } elseif (method_exists($resource, '__toString')) {
                 return stream_for((string) $resource, $options);
-            }
+            }return new PumpStream(function () use ($resource) {
+                if (!$resource->valid()) {
+                    return false;
+                }
+                $result = $resource->current();
+                $resource->next();
+                return $result;
+            }, $options);
             break;
         case 'NULL':
             return new Stream(fopen('php://temp', 'r+'), $options);
