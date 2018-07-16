07921b23dcef137247f9fc296fe541180121ea43
yusufSetiawan <yusuf.setiawan@kly.id>
2018-07-16 16:45:05 +0700

    fix error schrutinizer after add function clearNonAscii

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -59,7 +59,7 @@ class Model
     protected function filterStringInstance($string)
     {
         if (!empty($string)) {
-            $string = self::convertNonAscii($string);
+            $string = $this->convertNonAscii($string);
             return htmlentities($string);
         }
     }
@@ -163,35 +163,46 @@ class Model
      */
     private function convertNonAscii($string)
     {
+        $search = $replace = array();
+
         // Replace Single Curly Quotes
         $search[]  = chr(226).chr(128).chr(152);
         $replace[] = "'";
         $search[]  = chr(226).chr(128).chr(153);
         $replace[] = "'";
+
         // Replace Smart Double Curly Quotes
         $search[]  = chr(226).chr(128).chr(156);
         $replace[] = '"';
         $search[]  = chr(226).chr(128).chr(157);
         $replace[] = '"';
+
         // Replace En Dash
         $search[]  = chr(226).chr(128).chr(147);
         $replace[] = '--';
+
         // Replace Em Dash
         $search[]  = chr(226).chr(128).chr(148);
         $replace[] = '---';
+
         // Replace Bullet
         $search[]  = chr(226).chr(128).chr(162);
         $replace[] = '*';
+
         // Replace Middle Dot
         $search[]  = chr(194).chr(183);
         $replace[] = '*';
+
         // Replace Ellipsis with three consecutive dots
         $search[]  = chr(226).chr(128).chr(166);
         $replace[] = '...';
+
         // Apply Replacements
         $string = str_replace($search, $replace, $string);
+
         // Remove any non-ASCII Characters
         $string = preg_replace("/[^\x01-\x7F]/", "", $string);
+
         return $string;
     }
 }
