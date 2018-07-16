83c6805198ce604b8673eaeeec3d0e69dd15d79b
yusufSetiawan <yusuf.setiawan@kly.id>
2018-07-16 16:31:38 +0700

    Add function clear non ascii charackter from string

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -59,6 +59,7 @@ class Model
     protected function filterStringInstance($string)
     {
         if (!empty($string)) {
+            $string = self::convertNonAscii($string);
             return htmlentities($string);
         }
     }
@@ -152,5 +153,45 @@ class Model
         }
 
         throw new \Exception("method $name not exist");
+    }
+    
+    /**
+     * Clean non ASCII char from string
+     *
+     * @param $string
+     * @return string
+     */
+    private function convertNonAscii($string)
+    {
+        // Replace Single Curly Quotes
+        $search[]  = chr(226).chr(128).chr(152);
+        $replace[] = "'";
+        $search[]  = chr(226).chr(128).chr(153);
+        $replace[] = "'";
+        // Replace Smart Double Curly Quotes
+        $search[]  = chr(226).chr(128).chr(156);
+        $replace[] = '"';
+        $search[]  = chr(226).chr(128).chr(157);
+        $replace[] = '"';
+        // Replace En Dash
+        $search[]  = chr(226).chr(128).chr(147);
+        $replace[] = '--';
+        // Replace Em Dash
+        $search[]  = chr(226).chr(128).chr(148);
+        $replace[] = '---';
+        // Replace Bullet
+        $search[]  = chr(226).chr(128).chr(162);
+        $replace[] = '*';
+        // Replace Middle Dot
+        $search[]  = chr(194).chr(183);
+        $replace[] = '*';
+        // Replace Ellipsis with three consecutive dots
+        $search[]  = chr(226).chr(128).chr(166);
+        $replace[] = '...';
+        // Apply Replacements
+        $string = str_replace($search, $replace, $string);
+        // Remove any non-ASCII Characters
+        $string = preg_replace("/[^\x01-\x7F]/", "", $string);
+        return $string;
     }
 }
